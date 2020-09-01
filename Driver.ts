a256155b9009b9c4ba7bc9f7e566421b47e56902
Ali Mohamad <alimohamad1122@gmail.com>
2020-08-31 23:05:43 -0400

    Merged all feats into Feats.ts, added all feats to Driver.

--- Driver.ts
+++ Driver.ts
@@ -1,4 +1,4 @@
-import { Acolyte, Charlatan, Criminal, Entertainer, FolkHero, GuildArtisan, Hermit, Noble, Knight, Outlander, Sage, Sailor, Pirate, Soldier, Urchin } from './src/Backgrounds/Background';
+import { PlayerCharacter } from './src/Base/PlayerCharacter';
 
 import { HillDwarf, MountainDwarf } from './src/Races/Dwarf';
 import { HighElf, WoodElf, DarkElf } from './src/Races/Elf';
@@ -10,13 +10,10 @@ import { BaseHuman, VariantHuman } from './src/Races/Human';
 import { HalfOrc } from './src/Races/HalfOrc';
 import { Tiefling } from './src/Races/Tiefling';
 
-import { PlayerCharacter } from './src/Base/PlayerCharacter';
+import { Acolyte, Charlatan, Criminal, Entertainer, FolkHero, GuildArtisan, Hermit, Noble, Knight, Outlander, Sage, Sailor, Pirate, Soldier, Urchin } from './src/Backgrounds/Background';
 
-import {MagicInitiate} from './src/Feats/Ez';
+import { Alert, Athlete, Actor, Charger, CrossbowExpert, DefensiveDuelist, DualWielder, DungeonDelver, Durable, ElementalAdept, Grappler, GreatWeaponMaster, Healer, HeavilyArmored, HeavyArmorMaster, InspiringLeader, KeenMind, LightlyArmored, Linguist, Lucky, MageSlayer, MagicInitiate, MartialAdept, MediumArmorMaster, Mobile, ModeratelyArmored, MountedCombatant, Observant, PolearmMaster, Resilient, RitualCaster, SavageAttacker, Sentinel, Sharpshooter, ShieldMaster, Skilled, Skulker, SpellSniper, TavernBrawler, Tough, WarCaster, WeaponMaster } from './src/Feats/Feat';
 
 let pc: PlayerCharacter = new PlayerCharacter(10, 10, 10, 10, 10, 10);
 
-let feat = new MagicInitiate("Wizard", ["MAGE ARMOR", "AID"], "BANE");
-feat.apply(pc);
-
-console.log(pc.spells);
+console.log(pc);
