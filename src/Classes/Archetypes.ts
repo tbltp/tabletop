e4534e0f521f5c020ec7496c21e931cd0af6f9e7
Ali Mohamad <alimohamad1122@gmail.com>
2020-09-20 23:29:27 -0400

    Rogue Template Done (implementation in Rogue.ts, function headers in Archetype, empty ClassTraits)

--- src/Classes/Archetypes.ts
+++ src/Classes/Archetypes.ts
@@ -821,7 +821,55 @@ export class RangerArchetype extends Archetype {
 
 }
 
-export class RogueArchetype extends Archetype { }
+export class RogueArchetype extends Archetype { 
+    static archetypeHelper = {
+        "THIEF": {
+            "3": RogueArchetype.thief3,
+            "9": RogueArchetype.thief9,
+            "13": RogueArchetype.thief13,
+            "17": RogueArchetype.thief17
+        },
+        "ASSASSIN": {
+            "3": RogueArchetype.assassin3,
+            "9": RogueArchetype.assassin9,
+            "13": RogueArchetype.assassin13,
+            "17": RogueArchetype.assassin17
+        },
+        "ARCANE TRICKSTER": {
+            "3": RogueArchetype.arcaneTrickster3,
+            "9": RogueArchetype.arcaneTrickster9,
+            "13": RogueArchetype.arcaneTrickster13,
+            "17": RogueArchetype.arcaneTrickster17
+        }
+    }
+
+    static thief3(pc: PlayerCharacter, params: LevelingParams){ }
+    
+    static thief9(pc: PlayerCharacter, params: LevelingParams){ }
+    
+    static thief13(pc: PlayerCharacter, params: LevelingParams){ }
+
+    static thief17(pc: PlayerCharacter, params: LevelingParams){ }
+    
+    static assassin3(pc: PlayerCharacter, params: LevelingParams){ }
+    
+    static assassin9(pc: PlayerCharacter, params: LevelingParams){ }
+    
+    static assassin13(pc: PlayerCharacter, params: LevelingParams){ }
+    
+    static assassin17(pc: PlayerCharacter, params: LevelingParams){ }
+
+    static arcaneTrickster3(pc: PlayerCharacter, params: LevelingParams){ }
+    
+    static arcaneTrickster9(pc: PlayerCharacter, params: LevelingParams){ }
+    
+    static arcaneTrickster13(pc: PlayerCharacter, params: LevelingParams){ }
+    
+    static arcaneTrickster17(pc: PlayerCharacter, params: LevelingParams){ }
+
+    
+
+}
 
 export class SorcererArchetype extends Archetype { }
 
--- src/Classes/Warlock.ts
+++ src/Classes/Warlock.ts
@@ -1,12 +1,148 @@
-import { PlayerClass, LevelingParams } from './PlayerClass';
+import { PlayerClass, LevelingParams, SpellSlotFactory } from './PlayerClass';
 import { PlayerCharacter } from '../Base/PlayerCharacter';
-import {ISpell, Spell} from '../Base/Interfaces';
-import * as ClassTraits from '../../Assets/ClassTraits.json';
-import * as Spells from '../../Assets/Spells.json';
+import { ResourceTrait } from '../Base/Interfaces';
+import * as SpellList from '../../Assets/SpellList.json';
+import { PaladinArchetype } from './Archetypes';
 
 export class Warlock extends PlayerClass {
 
+    constructor(skillProficiencies: string[], weapons: string[], armor: string[], params: LevelingParams, equipmentPack: string){ 
+        super(
+            "Paladin",
+            [],
+            skillProficiencies, 
+            ["Simple", "Martial"],
+            ["Light", "Medium", "Heavy", "Shield"],
+            [],
+            weapons,
+            armor,
+            [],
+            [],
+            params,
+            "d10",
+            10,
+            ["wisdom", "charisma"]
+        );
 
-    abilitiesAtLevels = {};
-    features = [];
+        this.equipmentPack = equipmentPack
+
+        for(let level in this.abilitiesAtLevels) {
+            const func: Function = this.abilitiesAtLevels[level];
+            this.abilitiesAtLevels[level] = func.bind(this);
+        }
+    }
+
+    /** TODO
+     * FIGHTING STYLE LVL2
+     */
+
+	paladinOath: string;
+
+    abilitiesAtLevels = {
+        "1": this.level1,
+        "2": this.level2,
+        "3": this.level3,
+        "4": this.level4,
+        "5": this.level5,
+        "6": this.level6,
+        "7": this.level7,
+        "8": this.level8,
+        "9": this.level9,
+        "10": this.level10,
+        "11": this.level11,
+        "12": this.level12,
+        "13": this.level13,
+        "14": this.level14,
+        "15": this.level15,
+        "16": this.level16,
+        "17": this.level17,
+        "18": this.level18,
+        "19": this.level19,
+        "20": this.level20,
+    }
+
+    pushPaladinFeatures(pc: PlayerCharacter, level: string) {
+        this.pushClassFeatures(pc, level, "PALADIN");
+    }
+
+    level1(pc: PlayerCharacter, params: LevelingParams): void {
+        
+    }
+
+    level2(pc: PlayerCharacter, params: LevelingParams): void {
+        
+    }
+
+    level3(pc: PlayerCharacter, params: LevelingParams): void {
+        
+    }
+    
+    level4(pc: PlayerCharacter, params: LevelingParams): void {
+       
+    }
+
+    level5(pc: PlayerCharacter, params: LevelingParams): void {
+        
+    }
+
+    level6(pc: PlayerCharacter, params: LevelingParams): void {
+        
+    }
+
+    level7(pc: PlayerCharacter, params: LevelingParams): void {
+       
+    }
+
+    level8(pc: PlayerCharacter, params: LevelingParams): void {
+        
+    }
+
+    level9(pc: PlayerCharacter, params: LevelingParams): void {
+        
+    }
+
+    level10(pc: PlayerCharacter, params: LevelingParams): void {
+        
+    }
+
+    level11(pc: PlayerCharacter, params: LevelingParams): void {
+        
+    }
+
+    level12(pc: PlayerCharacter, params: LevelingParams): void {
+        
+    }
+
+    level13(pc: PlayerCharacter, params: LevelingParams): void {
+        
+    }
+
+    level14(pc: PlayerCharacter, params: LevelingParams): void {
+        
+    }
+
+    level15(pc: PlayerCharacter, params: LevelingParams): void {
+        
+    }
+
+    level16(pc: PlayerCharacter, params: LevelingParams): void {
+        
+    }
+
+    level17(pc: PlayerCharacter, params: LevelingParams): void {
+        
+    }
+
+    level18(pc: PlayerCharacter, params: LevelingParams): void {
+        
+    }
+
+    level19(pc: PlayerCharacter, params: LevelingParams): void {
+        
+    }
+
+    level20(pc: PlayerCharacter, params: LevelingParams): void {
+        
+    }
+    
 }
