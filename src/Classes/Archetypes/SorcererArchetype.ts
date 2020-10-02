88bf1be561bb348174381ae0f91114ef8af611f8
Ali Mohamad <alimohamad1122@gmail.com>
2020-10-02 18:21:00 -0400

    Finished Ranger, Rogue, Sorcerer (ish) Archetypes, up till Sorcerer in ClassTraits.json

--- src/Classes/Archetypes/SorcererArchetype.ts
+++ src/Classes/Archetypes/SorcererArchetype.ts
@@ -17,21 +17,46 @@ export class SorcererArchetype extends Archetype {
         "18": SorcererArchetype.wildMagic18,
       },
     };
+
+    /** TO DO
+     * Draconic Resilience: HpMax++ every level, add armor class
+     * Dragon Ancestor: Figure out how to reflect choice, and damage in one line.
+     */
+
+    static getFeature(archetypeName: string, level: string, featureName: string) {
+      return Archetype.getFeature("SORCERER", archetypeName, level, featureName);
+    }
   
-    static draconicAncestry1(pc: PlayerCharacter, params: LevelingParams) {}
+    static draconicAncestry1(pc: PlayerCharacter, params: LevelingParams) {
+
+    }
   
-    static draconicAncestry6(pc: PlayerCharacter, params: LevelingParams) {}
+    static draconicAncestry6(pc: PlayerCharacter, params: LevelingParams) {
+      pc.addFeatures(SorcererArchetype.getFeature("DRACONIC ANCESTRY", "6", "ELEMENTAL AFFINITY"))
+    }
   
-    static draconicAncestry14(pc: PlayerCharacter, params: LevelingParams) {}
+    static draconicAncestry14(pc: PlayerCharacter, params: LevelingParams) {
+      pc.addFeatures(SorcererArchetype.getFeature("DRACONIC ANCESTRY", "14", "DRAGON WINGS"))
+    }
   
-    static draconicAncestry18(pc: PlayerCharacter, params: LevelingParams) {}
+    static draconicAncestry18(pc: PlayerCharacter, params: LevelingParams) {
+      pc.addFeatures(SorcererArchetype.getFeature("DRACONIC ANCESTRY", "18", "DRACONIC PRESENCE"))
+    }
   
-    static wildMagic1(pc: PlayerCharacter, params: LevelingParams) {}
+    static wildMagic1(pc: PlayerCharacter, params: LevelingParams) {
+      pc.addFeatures(SorcererArchetype.getFeature("WILD MAGIC", "1", "WILD MAGIC SURGE"), SorcererArchetype.getFeature("WILD MAGIC", "3", "TIDES OF CHAOS"))
+    }
   
-    static wildMagic6(pc: PlayerCharacter, params: LevelingParams) {}
+    static wildMagic6(pc: PlayerCharacter, params: LevelingParams) {      
+      pc.addFeatures(SorcererArchetype.getFeature("WILD MAGIC", "6", "BEND LUCK"))
+    }
   
-    static wildMagic14(pc: PlayerCharacter, params: LevelingParams) {}
+    static wildMagic14(pc: PlayerCharacter, params: LevelingParams) {
+      pc.addFeatures(SorcererArchetype.getFeature("WILD MAGIC", "14", "CONTROLLED CHAOS"))
+    }
   
-    static wildMagic18(pc: PlayerCharacter, params: LevelingParams) {}
+    static wildMagic18(pc: PlayerCharacter, params: LevelingParams) {
+      pc.addFeatures(SorcererArchetype.getFeature("WILD MAGIC", "3", "SPELL BOMBARDMENT"))
+    }
 }
   
