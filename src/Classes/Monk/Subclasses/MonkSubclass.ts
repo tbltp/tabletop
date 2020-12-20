8e496cd8222c73cbb43bc44448961fd67678051e
Johnathan <shiaodustin@gmail.com>
2020-12-20 15:45:41 -0500

    Removed artifacts from merging

--- src/Classes/Monk/Subclasses/MonkSubclass.ts
+++ src/Classes/Monk/Subclasses/MonkSubclass.ts
@@ -1,10 +1,8 @@
 import { Subclass } from "../../Subclass";
-<<<<<<< HEAD
-import { OpenHand } from "./OpenHand/OpenHand"
-import { Shadow } from "./Shadow/Shadow"
-import { FourElements } from "./FourElements/FourElements"
-import { DrunkenMaster} from "./DrunkenMaster/DrunkenMaster"
-import { Kensei} from "./Kensei/Kensei"
+import { OpenHand } from "./OpenHand/OpenHand";
+import { Shadow } from "./Shadow/Shadow";
+import { FourElements } from "./FourElements/FourElements";
+import { Kensei } from "./Kensei/Kensei";
 
 export class MonkSubclass extends Subclass {
   constructor(subclass: string){
@@ -24,6 +22,12 @@ export class MonkSubclass extends Subclass {
       "11": Shadow.shadow11,
       "17": Shadow.shadow17
     },
+    KENSEI : {
+      "3": Kensei.kensei3,
+      "6": Kensei.kensei6,
+      "11": Kensei.kensei11,
+      "17": Kensei.kensei17
+    },
     "FOUR ELEMENTS": {
       "3": FourElements.fourElements3,
       "5": FourElements.fourElements5,
@@ -32,58 +36,6 @@ export class MonkSubclass extends Subclass {
       "11": FourElements.fourElements11,
       "13": FourElements.fourElements13,
       "17": FourElements.fourElements17
-    },
-    "DRUNKEN MASTER": {
-      "3": DrunkenMaster.drunkenMaster3,
-      "6": DrunkenMaster.drunkenMaster6,
-      "11": DrunkenMaster.drunkenMaster11,
-      "17": DrunkenMaster.drunkenMaster17
-    },
-    KENSEI: {
-      "3": Kensei.kensei3,
-      "6": Kensei.kensei6,
-      "11": Kensei.kensei11,
-      "17": Kensei.kensei17
-=======
-import { OpenHand } from "./OpenHand/OpenHand";
-import { Shadow } from "./Shadow/Shadow";
-import { FourElements } from "./FourElements/FourElements";
-import { Kensei } from "./Kensei/Kensei";
-
-export class MonkSubclass extends Subclass {
-    constructor(subclass: string){
-      super(subclass);
-    }
-    
-    subclassDictionary = {
-      "OPEN HAND": {
-        "3": OpenHand.openHand3,
-        "6": OpenHand.openHand6,
-        "11": OpenHand.openHand11,
-        "17": OpenHand.openHand17
-      },
-      SHADOW: {
-        "3": Shadow.shadow3,
-        "6": Shadow.shadow6,
-        "11": Shadow.shadow11,
-        "17": Shadow.shadow17
-      },
-      KENSEI : {
-        "3": Kensei.kensei3,
-        "6": Kensei.kensei6,
-        "11": Kensei.kensei11,
-        "17": Kensei.kensei17
-      },
-      "FOUR ELEMENTS": {
-        "3": FourElements.fourElements3,
-        "5": FourElements.fourElements5,
-        "6": FourElements.fourElements6,
-        "9": FourElements.fourElements9,
-        "11": FourElements.fourElements11,
-        "13": FourElements.fourElements13,
-        "17": FourElements.fourElements17
-      }
->>>>>>> 377d0f5e8a63da31eb75ae38fcdd8f1a7708804f
     }
   }
 }
