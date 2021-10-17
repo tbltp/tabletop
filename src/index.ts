e290d509aa59c3bfaec5c9d6515bbcaa426c669a
Ali Mohamad <alimohamad1122@gmail.com>
2021-10-17 16:06:17 -0700

    Feature/choice build (#143)
    * Class Choices rebuild.
    
    * Testing Nested Choices
    
    * testing bard
    
    * smaller choice docs + bug in class prop setting
    
    * ignoring delete folder
    
    * delete delete folder (backed up on notion)
    
    * fixed delete
    
    * Fixed index

--- src/index.ts
+++ src/index.ts
@@ -104,7 +104,7 @@ export { Jsonify } from "../src/Utilities/Jsonify";
 
 export { CharacterSheet } from "../src/Base/CharacterSheet";
 
-export { PlayerFactory } from "../src/Utilities/Choices";
+export { PlayerFactory } from "../src/Utilities/Choice Build Specs/Choices";
 /*
 export { CLISheetDriver } from "../src/Utilities/CLISheetDriver";
 */
