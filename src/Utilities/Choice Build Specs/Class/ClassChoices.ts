3d8b9d74253cc4d542a0d97439ba61fe0643e7fb
Ali Mohamad <alimohamad1122@gmail.com>
2021-10-17 14:56:39 -0700

    smaller choice docs + bug in class prop setting

---
+++ src/Utilities/Choice Build Specs/Class/ClassChoices.ts
@@ -0,0 +1,27 @@
+ import * as BarbarianChoices from './Barbarian.json'
+ import * as BardChoices from './Bard.json'
+ import * as ClericChoices from './Cleric.json'
+ import * as DruidChoices from './Druid.json'
+ import * as FighterChoices from './Fighter.json'
+ import * as MonkChoices from './Monk.json'
+ import * as PaladinChoices from './Paladin.json'
+ import * as RangerChoices from './Ranger.json'
+ import * as RogueChoices from './Rogue.json'
+ import * as SorcererChoices from './Sorcerer.json'
+ import * as WarlockChoices from './Warlock.json'
+ import * as WizardChoices from './Wizard.json'
+ 
+ export const ClassChoices = {
+    Barbarian: BarbarianChoices,
+    Bard: BardChoices,
+    Cleric: ClericChoices,
+    Druid: DruidChoices,
+    Fighter: FighterChoices,
+    Monk: MonkChoices,
+    Paladin: PaladinChoices,
+    Ranger: RangerChoices,
+    Rogue: RogueChoices,
+    Sorcerer: SorcererChoices,
+    Warlock: WarlockChoices,
+    Wizard: WizardChoices
+}
