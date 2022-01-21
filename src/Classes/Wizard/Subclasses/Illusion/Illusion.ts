806fb7a5b4e3cb78fce2f6544ca7eaa64dd808ec
Ali Mohamad <alimohamad1122@gmail.com>
2022-01-20 23:28:24 -0800

    Minor Illusion fix in School of Illusion subclass

--- src/Classes/Wizard/Subclasses/Illusion/Illusion.ts
+++ src/Classes/Wizard/Subclasses/Illusion/Illusion.ts
@@ -1,6 +1,7 @@
-import { PlayerCharacter } from "../../../../Base/PlayerCharacter";
-import { LevelingParams } from "../../../../Classes/PlayerClass";
 import * as IllusionSchoolDict from "./Illusion.json"
+
+import { LevelingParams } from "../../../../Classes/PlayerClass";
+import { PlayerCharacter } from "../../../../Base/PlayerCharacter";
 
 export class Illusion {
 
@@ -10,6 +11,20 @@ export class Illusion {
 
     static illusion2(pc: PlayerCharacter, params: LevelingParams): void {
         pc.pcHelper.addFeatures(Illusion.getFeature("2", "ILLUSION SAVANT"), Illusion.getFeature("2", "IMPROVED MINOR ILLUSION"))
+        
+        // Handle Minor Illusion or Free Cantrip
+        if(pc.spells["0"].find(spell => spell.name === "Minor Illusion")){
+            pc.pcHelper.addSpells(
+                [...params.subclassParams.spellSelections.add], 
+                "intelligence"
+            )
+        }
+        else {
+            pc.pcHelper.addSpells(
+                ["MINOR ILLUSION"], 
+                "intelligence"
+            )
+        }
     }
     
     static illusion6(pc: PlayerCharacter, params: LevelingParams): void {
