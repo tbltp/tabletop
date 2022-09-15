eae90f9033c06e5c4ccc3f121908b7a4fc738ee2
Ez Ablaza <ezra.ablaza.inbox@gmail.com>
2022-09-15 01:44:00 -0500

    add utility to check for case independent matching

--- src/Utilities/General.ts
+++ src/Utilities/General.ts
@@ -1,12 +1,19 @@
 function searchInDictionary(key: string, dictionary: object): any | undefined {
-    /*
-    Takes in a key and a dictionary and returns the corresponding value if found
-    otherwise returns undefined
-    */
+    /**
+     * Takes in a key and a dictionary and returns the corresponding value if found
+     * otherwise returns undefined
+     */
     if (key in dictionary) {
         return dictionary[key];
     }
     return undefined;
 }
 
-export { searchInDictionary };
+function isEqualIgnoreCase(str1: string, str2: string): boolean {
+    /**
+     * Checks if two strings match regardless of casing
+     */
+    return str1.toLowerCase() === str2.toLowerCase();
+}
+
+export { searchInDictionary, isEqualIgnoreCase };
